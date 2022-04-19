e8115b201c03e22381b260fc459623883c5e1a45
Emir <klerni89@gmail.com>
2022-04-19 19:03:23 +0600

    site (3 /) added

---
+++ blogs/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+
+def blog_index(request):
+    blogs = [
+        {
+            'name': 'The first blog',
+            'author': {'name': 'admin'},
+            'publication_date': '16.04.2022'
+        }, 
+        {
+            'name': 'The second blog',
+            'author': {'name': 'admin'},
+            'publication_date': '12.04.2022'
+        }
+        ]
+    context = {
+        'blogs': blogs
+    }
+    return render(request, 'blogs/blog_index.html', context=context)
+
+
+def user_page(request):
+    user_data = {
+        'user_name': 'Emir',
+        'user_age': 14
+    }
+    context = {
+        'user_data': user_data
+    }
+    return render(request,  'blogs/user_page.html', context=context)
+
+
+def first_blog(request):
+    context = {
+
+    }
+    return render(request, 'blogs/first_blog.html', context=context)
